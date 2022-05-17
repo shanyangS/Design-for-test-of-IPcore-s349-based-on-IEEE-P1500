58908022f0959d8211baa00ce14f314f8aee4879
ShanDaYang <shanyangs@outlook.com>
2022-05-17 17:10:09 +0800

    Final version

--- ip349_verilog/sel_gen.v
+++ ip349_verilog/sel_gen.v
@@ -1,10 +1,35 @@
 module sel_gen
 (
-	input wby_shift,wir_wpc,wir_extest,wir_intest,
-	output[3:0] sel_o,
+	input wir_bypass,wir_wpc,wir_extest,wir_intest,
+	output reg mux01,mux02,mux03,mux04,mux05,mux06,mux07,mux08,SE,HE
 );
 
-always@(*)
-	if(wby_shift = 1)
 
+always@(*)
+begin
+	if(wir_bypass == 1)
+	begin
+	mux01 <= 0; mux02 <= 0; mux03 <= 0; mux04 <= 0; 
+	mux05 <= 0; mux06 <= 0; mux07 <= 0; mux08 <= 0; 
+	SE = 0; HE = 1;
+	end
+	if(wir_wpc == 1)
+	begin
+	mux01 <= 0; mux02 <= 0; mux03 <= 0; mux04 <= 1; 
+	mux05 <= 1; mux06 <= 1; mux07 <= 0; mux08 <= 0; 
+	SE = 1; HE = 0;
+	end
+	if(wir_extest == 1)
+	begin
+	mux01 <= 1; mux02 <= 1; mux03 <= 1; mux04 <= 1; 
+	mux05 <= 1; mux06 <= 1; mux07 <= 1; mux08 <= 1; 
+	SE = 0; HE = 1;
+	end
+	if(wir_intest == 1)
+	begin
+	mux01 <= 1; mux02 <= 1; mux03 <= 1; mux04 <= 1; 
+	mux05 <= 1; mux06 <= 1; mux07 <= 1; mux08 <= 1; 
+	SE = 1; HE = 0;
+	end
+end
 endmodule
--- ip349_verilog/top.v
+++ ip349_verilog/top.v
@@ -3,7 +3,7 @@ module top
 	input GND,VDD,CK,START,
 	B0,B1,B2,B3,A0,A1,A2,A3,
 	wby_si,wby_shift,
-	input[2:0] wir_wsi,
+	input[2:0] wir_wsi,WPI,WPO,
 	input WRSTN,WRCK,SelectWIR,ShiftWR,CaptureWR,UpdateWR,
 	output wir_wso,CNTVCO2,CNTVCON2,READY,P0,P1,P2,P3,P4,P5,P6,P7
 );
@@ -13,24 +13,47 @@ wby_wire,wir_intest,hold_inputs,hold_outputs;
 
 wire CNTVCO2_wire,CNTVCON2_wire,READY_wire,
 P7_wire,P6_wire,P5_wire,P4_wire,P3_wire,P2_wire,P1_wire,
-P0_wire;
+P0_wire,wire_muxz1;
 
-s349 kn1(.GND(GND),.VDD(VDD),.CK(CK),.START(START),
+s349 kn1
+(
+.GND(GND),.VDD(VDD),.CK(CK),.START(START),
 .B0(B0),.B1(B1),.B2(B2),.B3(B3),
 .A0(A0),.A1(A1),.A2(A2),.A3(A3),
 .CNTVCO2(CNTVCO2),.CNTVCON2(CNTVCON2),.READY(READY),
-.P0(P0),.P1(P1),.P2(P2),.P3(P3),.P4(P4),.P5(P5),.P6(P6),.P7(P7));
+.P0(P0),.P1(P1),.P2(P2),.P3(P3),.P4(P4),.P5(P5),.P6(P6),.P7(P7)
+);
 
-WIR p1(.CaptureWR(CaptureWR),.WPSE(WPSE),.UpdateWR(UpdateWR),
+sel_gen n1
+(
+.wir_bypass(wir_bypass),.wir_wpc(wir_wpc),
+.wir_extest(wir_extest),.wir_intest(wir_intest),
+.mux01(mux01),.mux02(mux02),.mux03(mux03),
+.mux04(mux04),.mux05(mux05),.mux06(mux06),
+.SE(SE),.HE(HE)
+);
+
+WIR p1
+(
+.CaptureWR(CaptureWR),.WPSE(WPSE),.UpdateWR(UpdateWR),
 .WRCK(CK),.WRSTN(WRSTN),.ShiftWR(ShiftWR),.SelectWIR(SelectWR)
 ,.wir_wsi(wir_wsi),.wir_wpc(wir_wpc),.wir_extest(wir_extest),
 .wir_intest(wir_intest),
-.hold_inputs(hold_inputs),.hold_outputs(hold_outputs),.wir_wso(wir_wso));
+.hold_inputs(hold_inputs),.hold_outputs(hold_outputs),.wir_wso(wir_wso)
+);
 
-mux21 p2(.a(),.b(),.s(),.y(mux21_wire));
-mux21 p3(.a(mux21_wire),.b(),.s(SelectWR),.y(WSO));
+mux21 z1(.a(CFO_19),.b(wby_wire),.s(wir_bypass),.y(wire_muxz1));
+mux21 z2(.a(),.b(),.s(),.y());
 
-WBY y1(.clk(CK),.wby_si(wby_si),.wby_shift(by_shift),.q(wby_wire));
+
+mux21 x1(.a(WPI[0]),.b(wir_intest),.s(mux01),.y(mux21_wire));
+mux21 x2(.a(WPI[1]),.b(CFO_5),.s(SelectWR),.y(WSO));
+mux21 x3(.a(WPI[2]),.b(CFO_8),.s(SelectWR),.y(CFI_0));
+mux21 x4(.a(CFO_8),.b(WPO[0]),.s(SelectWR),.y(CFI_5));
+mux21 x5(.a(CFO_12),.b(WPO[1]),.s(SelectWR),.y(CFI_8));
+mux21 x6(.a(CFO_12),.b(WPO[2]),.s(SelectWR),.y(CFI_12));
+
+WBY y1(.clk(CK),.wby_si(wby_si),.wby_shift(wby_shift),.q(wby_wire));
 
 WBR a0(.SE(SE),.HE(HE),.CFI(CFI),.CTI(A0),.clk(CK),.CFO(CFO_0),.CTO(A0_wire));
 WBR a1(.SE(SE),.HE(HE),.CFI(CFO_0),.CTI(A1),.clk(CK),.CFO(CFO_1),.CTO(A1_wire));
@@ -48,32 +71,9 @@ WBR a12(.SE(SE),.HE(HE),.CFI(CFO_11),.CTI(P7_wire),.clk(CK),.CFO(CFO_12),.CTO(P7
 WBR a13(.SE(SE),.HE(HE),.CFI(CFO_12),.CTI(P6_wire),.clk(CK),.CFO(CFO_13),.CTO(P6));
 WBR a14(.SE(SE),.HE(HE),.CFI(CFO_13),.CTI(P5_wire),.clk(CK),.CFO(CFO_14),.CTO(P5));
 WBR a15(.SE(SE),.HE(HE),.CFI(CFO_14),.CTI(P4_wire),.clk(CK),.CFO(CFO_15),.CTO(P4));
-WBR a16(.SE(SE),.HE(HE),.CFI(CFO_0),.CTI(P3_wire),.clk(CK),.CFO(),.CTO(P3));
-WBR a17(.SE(SE),.HE(HE),.CFI(CFO_0),.CTI(P2_wire),.clk(CK),.CFO(),.CTO(P2));
-WBR a18(.SE(SE),.HE(HE),.CFI(CFO_0),.CTI(P1_wire),.clk(CK),.CFO(),.CTO(P1));
-WBR a19(.SE(SE),.HE(HE),.CFI(CFO_0),.CTI(P0_wire),.clk(CK),.CFO(),.CTO(P0));
+WBR a16(.SE(SE),.HE(HE),.CFI(CFO_15),.CTI(P3_wire),.clk(CK),.CFO(CFO_16),.CTO(P3));
+WBR a17(.SE(SE),.HE(HE),.CFI(CFO_16),.CTI(P2_wire),.clk(CK),.CFO(CFO_17),.CTO(P2));
+WBR a18(.SE(SE),.HE(HE),.CFI(CFO_17),.CTI(P1_wire),.clk(CK),.CFO(CFO_18),.CTO(P1));
+WBR a19(.SE(SE),.HE(HE),.CFI(CFO_18),.CTI(P0_wire),.clk(CK),.CFO(CFO_19),.CTO(P0));
 
-/*
-module WBY
-(input clk,wby_si,wby_shift,
- output q
-);
-
- wire wby_so;
-
- D_FF i0(.clk(clk),.d(wby_so),.q(q));
- mux21 y0(.a(q),.b(wby_si),.s(wby_shift),.y(wby_so));
-
-
-endmodule
-*/
-/*
-module WIR
-(
-	input CaptureWR,WPSE,UpdateWR,WRCK,WRSTN,ShiftWR,SelectWIR,
-	input[2:0] wir_wsi,
-	output wir_wpc,wir_extest,wir_intest,hold_inputs,hold_outputs,wir_wso
-	
-);
-*/
 endmodule
